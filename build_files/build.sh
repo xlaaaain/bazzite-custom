cf0028d17a47ef2718bbba6ccfcdca6acf73bc33
Laaaain <laaaain@0x00.network>
2026-02-25 15:58:40 -0600

    feat: Added KDE applications for email, calendar and contacts.

--- build_files/build.sh
+++ build_files/build.sh
@@ -54,7 +54,15 @@ dnf5 install -y podman-compose \
   vlc-plugins-all \
   konsole \
   neovim \
-  htop
+  htop \
+  kmail \
+  korganizer \
+  kalarm \
+  kaddressbook \
+  kde-partitionmanager \
+  kdepim-runtime \
+  kdepim-addons \
+  kontact
 
 # install from bazzite-multilib copr
 dnf5 -y install --repo="copr:copr.fedorainfracloud.org:ublue-os:bazzite-multilib" \
